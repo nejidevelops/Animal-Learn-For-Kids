f98072716d7c3f68170bdb6c2c3f4d69368a989f
NewtonOmbese <newtonombese1@gmail.com>
2022-06-23 08:01:00 +0300

    getting my api ready to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,23 @@ const animalPic = "http://localhost:3000/characters"
 function getOurAnimals(){
     fetch (animalPic)
     .then(res=>res.json())
-    .then(data => data.forEach((newAnimals) => {
-        console.log(newAnimals.name)
+    .then(data => data.forEach((newData) => {
+        console.log(newData.name)
+        ourAnimalNames(newData)
     }))
+}
+
+function ourAnimalNames(anime){
+    let spanified = document.createElement('span')
+    spanified.innerHTML = anime.name;
+    let p = document.createElement('p').appendChild(spanified)
+    p.setAttribute('id', 'anime-name')
+    document.getElementById('animals').appendChild(p)
+
+    p.addEventListener('click', () =>{
+        const animalName = document.getElementById('animalName')
+        const animalImage = document.getElementById('animalImage')
+        animalName.innerHTML = anime.name
+        animalImage.setAttribute('src', `${anime.image}`)
+    })
 }
